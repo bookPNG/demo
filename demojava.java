df4c4a20a0b890c97ef61f42dcd98e7201edac62
PNGx <61050299@kmitl.ac.th>
2020-07-21 16:47:58 +0700

    first commit

---
+++ demojava.java
@@ -0,0 +1,9 @@
+class demojava{
+    public static void main(String[] args){
+        System.out.println("Hello java");
+        System.out.print(zxc(2, 3, 4));
+    }
+    static int zxc(int n1, int n2, int n3){
+        return n1*n2+n3;
+    }
+}
---
+++ demopy.py
@@ -0,0 +1,5 @@
+def zxc(n1, n2, n3):
+    return n1*n2+n3
+
+print("Hello python")
+print(zxc(2, 3, 4)) 
